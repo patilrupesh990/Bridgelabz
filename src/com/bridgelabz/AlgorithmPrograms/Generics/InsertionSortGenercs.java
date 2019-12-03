68494cf44072b8819b69901fedf6849ca03d0b5d
Rupesh <patilrupesh990@gmail.com>
2019-12-03 12:03:54 +0530

    Insertionsort updated

--- src/com/bridgelabz/AlgorithmPrograms/Generics/InsertionSortGenercs.java
+++ src/com/bridgelabz/AlgorithmPrograms/Generics/InsertionSortGenercs.java
@@ -10,22 +10,22 @@ import java.util.ArrayList;
 
 public class InsertionSortGenercs 
 {
-	static Utility util=new Utility();
+
 	public static void main(String[] args) 
 	{
 		ArrayList<String> al=new ArrayList<String>();
 	
 		System.out.println("How many words wants to insert??");
-		int size=util.InputInt();
+		int size=Utility.InputInt();
 		
 		System.out.println("Enter the words into list");
 		for(int i=0;i<size;i++)
 		{
-			String s=util.InputString();
+			String s=Utility.InputString();
 			al.add(s);
 		}
 		
-		util.InsertionSort(al);
+		Utility.InsertionSort(al);
 	}
 	
 }
