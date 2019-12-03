7ef0d581d6700dc35797dd183fb23cbfe1550be3
Rupesh <patilrupesh990@gmail.com>
2019-12-03 12:40:07 +0530

    TicTacToe Updated

--- src/com/bridgelabz/logicalprograms/TicTacToe.java
+++ src/com/bridgelabz/logicalprograms/TicTacToe.java
@@ -10,11 +10,12 @@ public class TicTacToe
 {
 	public static void main(String[] args) 
 	{
-		Utility util=new Utility();
-		com.bridgelabz.util.Utility scan=new com.bridgelabz.util.Utility();
+		
+		
 		System.out.println("Welcome to TicTacToe its an amazing Game");
 		System.out.println("Enter Your Name:");
-		String name=scan.InputString();
+		String name=com.bridgelabz.util.Utility.InputString();
+		Utility util=new Utility();
 		util.Initilization();
 		util.PrintBord(name);
 	}
